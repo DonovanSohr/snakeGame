f21bdb2ef4590f492820063ae85c2485377b6a25
Donovan Sohr <donovansohr@ool-ad0201ba.dyn.optonline.net>
2018-10-26 10:40:26 -0400

    got score to work

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const context = canvas.getContext('2d');
 let grid = 16;
 let count = 0;
 let score = 1;
+let newScore = document.querySelector('.score');
 
 let snake = {
   x: 160,
@@ -67,10 +68,10 @@ function loop() {
 
     if (cell.x === food.x && cell.y === food.y) {
       snake.startUnits += 5;
-      score += 5
-//ADD SCORE
       food.x = makeItRandom(0, 62.5) * grid;
       food.y = makeItRandom(0, 43.75) * grid;
+      score += 5
+        newScore.innerText = score;
     }
 
     for (let i = index + 1; i < snake.units.length; i += 1) {
@@ -115,3 +116,140 @@ document.onkeydown = function (event) {
   }
 }
 requestAnimationFrame(loop);
+
+// let gameSpeed = 60;
+// let canvasBorderColor = 'lightgreen';
+// let canvasBackgroundColor = "black";
+// let snakeColor = 'lightgreen';
+// let snakeBorderColor = 'black';
+// let foodColor = 'red';
+// let foodBorderColor = 'black';
+//
+// let score = 1;
+// let changeDirec = false;
+// let foodX;
+// let foodY;
+
+
+// main();
+//
+// createFood();
+//
+// document.addEventListener("keydown", changeDirection);
+//
+// function main() {
+//
+//   if (didGameEnd()) return;
+//   setTimeout(function onEnd() {
+//     changeDirec = false;
+//     clearCanvas();
+//     drawFood();
+//     advanceSnake();
+//     drawSnake();
+//
+//     main();
+//   }, gameSpeed)
+// }
+//
+// function clearCanvas() {
+//   ctx.fillStyle = canvasBackgroundColor;
+//   ctx.strokestyle = canvasBorderColor;
+//   ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+//   ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+// }
+//
+// function drawFood() {
+//   ctx.fillStyle = foodColor;
+//   ctx.strokestyle = foodBorderColor;
+//   ctx.fillRect(foodX, foodY, 20, 20);
+//   ctx.strokeRect(foodX, foodY, 20, 20);
+// }
+//
+// function advanceSnake() {
+//   let head = {x: snake[0].x + dx, y: snake[0].y + dy};
+//   snake.unshift(head);
+//
+//   let didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+//   if (didEatFood) {
+//     score += 5;
+//     document.getElementById('score').innerHTML = score;
+//     createFood();
+//   }
+//   else {
+//     snake.pop();
+//   }
+// }
+//
+// function didGameEnd() {
+//   for (let i = 4; i < snake.length; i++) {
+//     if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+//   }
+//
+//   let hitLeftWall = snake[0].x < 0;
+//   let hitRightWall = snake[0].x > gameCanvas.width - 10;
+//   let hitToptWall = snake[0].y < 0;
+//   let hitBottomWall = snake[0].y > gameCanvas.height - 10;
+//   return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
+// }
+//
+// function drawSnake() {
+//   snake.forEach(drawSnakePart)
+// }
+//
+// function drawSnakePart(snakePart) {
+//   ctx.fillStyle = snakeColor;
+//   ctx.strokestyle = snakeBorderColor;
+//   ctx.fillRect(snakePart.x, snakePart.y, 20, 20);
+//   ctx.strokeRect(snakePart.x, snakePart.y, 20, 20);
+// }
+//
+// function randomTen(min, max) {
+//   return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+// }
+//
+// function createFood() {
+//   foodX = randomTen(0, gameCanvas.width - 10);
+//   foodY = randomTen(0, gameCanvas.height - 10);
+//   snake.forEach(function isFoodOnSnake(part) {
+//     const foodIsoNsnake = part.x === foodX && part.y === foodY;
+//     if (foodIsoNsnake) createFood();
+//   });
+// }
+//
+//  document.onkeydown = function (event) {
+//     keyCode = window.event.keyCode;
+//     keyCode = event.keyCode;
+//   }
+//
+// function changeDirection(event) {
+//   const left = 37;
+//   const right = 39;
+//   const up = 38;
+//   const down = 40;
+//
+//   if (changeDirec) return;
+//   changeDirec = true;
+//
+//   let attemptToChange = event.keyCode;
+//   let goingUp = dy === -10;
+//   let goingDown = dy === 10;
+//   let goingRight = dx === 10;
+//   let goingLeft = dx === -10;
+//
+//   if (attemptToChange === left && !goingRight) {
+//     dx = -10;
+//     dy = 0;
+//   }
+//   if (attemptToChange === up && !goingDown) {
+//     dx = 0;
+//     dy = -10;
+//   }
+//   if (attemptToChange === right && !goingLeft) {
+//     dx = 10;
+//     dy = 0;
+//   }
+//   if (attemptToChange === down && !goingUp) {
+//     dx = 0;
+//     dy = 10;
+//   }
+// }
